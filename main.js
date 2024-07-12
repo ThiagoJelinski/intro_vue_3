5ba7f1d1f31107002e25415fdb4a32b7b9887fbd
Thiago Jelinski <thi.jel@outlook.com>
2024-07-12 20:19:13 -0300

    Vinculação de Classe e Estilo

--- main.js
+++ main.js
@@ -3,13 +3,15 @@ const app = Vue.createApp({
         return {
             cart: 0,
             product: 'Socks',
+            brand: 'Vue Mastery',
             image: './assets/images/socks_green.jpg',
             inventory: 5,
             onSale: true,
+            inStock: true,
             details: ['50% algodão', '30% lã', '20% poliester'],
             variants: [
-                { id: 2234, color: 'verde', image: './assets/images/socks_green.jpg' },
-                { id: 2235, color: 'azul', image: './assets/images/socks_blue.jpg' },
+                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
+                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
             ],
             sizes: ['S','M','L','XL'],
             url: 'https://github.com/csp1po/intro_vue_3/tree/t3-start',
