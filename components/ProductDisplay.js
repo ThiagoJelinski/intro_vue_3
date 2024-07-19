b40f591c2c57aee4bbfeb3220b20e782bbd90b31
Thiago Jelinski <thi.jel@outlook.com>
2024-07-19 10:54:24 -0300

    Formularios e V-models

--- components/ProductDisplay.js
+++ components/ProductDisplay.js
@@ -9,41 +9,44 @@ app.component('product-display', {
   /*html*/
   `<div class="product-display">
     <div class="product-container">
-      <div class="product-image">
-        <img v-bind:src="image">
-      </div>
-      <div class="product-info">
-        <h1>{{ title }}</h1>
-
-        <p v-if="inStock">In Stock</p>
-        <p v-else>Out of Stock</p>
-
-        <p>Shipping: {{ shipping }}</p>
-        <ul>
-          <li v-for="detail in details">{{ detail }}</li>
-        </ul>
-
-        <div 
-          v-for="(variant, index) in variants" 
-          :key="variant.id" 
-          @mouseover="updateVariant(index)" 
-          class="color-circle" 
-          :style="{ backgroundColor: variant.color }">
-        </div>
-        
-        <button 
-          class="button" 
-          :class="{ disabledButton: !inStock }" 
-          :disabled="!inStock" 
-          v-on:click="addToCart">
-          Add to Cart
-        </button>
-
-      </div>
+    <div class="product-image">
+    <img v-bind:src="image">
+    </div>
+    <div class="product-info">
+    <h1>{{ title }}</h1>
+    
+    <p v-if="inStock">In Stock</p>
+    <p v-else>Out of Stock</p>
+    
+    <p>Shipping: {{ shipping }}</p>
+    <ul>
+    <li v-for="detail in details">{{ detail }}</li>
+    </ul>
+    
+    <div 
+    v-for="(variant, index) in variants" 
+    :key="variant.id" 
+    @mouseover="updateVariant(index)" 
+    class="color-circle" 
+    :style="{ backgroundColor: variant.color }">
+    </div>
+    
+    <button 
+    class="button" 
+    :class="{ disabledButton: !inStock }" 
+    :disabled="!inStock" 
+    v-on:click="addToCart">
+    Add to Cart
+    </button>
+    
+    </div>
+    <review-list :reviews="reviews"></review-list>
+    <review-form @review-submitted="addReview"></review-form>
     </div>
   </div>`,
   data() {
     return {
+        reviews:[],
         product: 'Socks',
         brand: 'Vue Mastery',
         selectedVariant: 0,
@@ -55,6 +58,9 @@ app.component('product-display', {
     }
   },
   methods: {
+      addReview(review){
+        this.review.push(review)
+      },
       addToCart() {
           this.$emit('add-to-cart', this.variants[this.selectedVariant].id)
       },
